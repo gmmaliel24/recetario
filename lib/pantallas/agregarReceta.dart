44dfd917d3b1061446937109bcac80ba9b0f31bd
Gamaliel <gaguilarl2@miumg.edu.gt>
2024-07-21 12:17:00 -0600

    Agregamos más pantallas

---
+++ lib/pantallas/agregarReceta.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class AgregaRreceta extends StatefulWidget {
+  @override
+  _AgregarRecetaState createState() => _AgregarRecetaState();
+}
+
+class _AgregarRecetaState extends State<AgregaRreceta> {
+  final TextEditingController _controllerNombreReceta = TextEditingController();
+  final TextEditingController _controllerDescripcionReceta =
+      TextEditingController();
+  final TextEditingController _controllerIngredientesReceta =
+      TextEditingController();
+  final TextEditingController _controllerInstruccionesReceta =
+      TextEditingController();
+  final TextEditingController _controllerTiempoReceta = TextEditingController();
+  File? _imageReceta;
+
+  void _guardarReceta() async {
+    try{
+      String nombreReceta = _controllerNombreReceta.text;
+      String descripcionReceta = _controllerDescripcionReceta.text;
+      String ingredientesReceta = _controllerIngredientesReceta.text;
+      String instruccionesReceta = _controllerInstruccionesReceta.text;
+      Int tiempoReceta = _controllerTiempoReceta;
+      File imagen = _imageReceta;
+
+      if(
+        nombreReceta.isEmpty || descripcionReceta.isEmpty || ingredientesReceta.isEmpty || instruccionesReceta.isEmpty || tiempoReceta || imagen == null
+      ){
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Por favor llene todos los campos')));
+      }
+    }catch(e){
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error al guardar toso')))
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: ElevatedButton(
+            onPressed: () {
+              Navigator.pop;
+            },
+            child: const Text(
+              'Regresar',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )),
+        title: const Center(
+          child: Text(
+            'Editar Receta',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+        actions: [
+          ElevatedButton(
+              onPressed: _guardarReceta,
+              child: const Row(
+                children: [
+                  Text(
+                    'Guardar',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  Icon(Icons.save)
+                ],
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const Text(
+                'Nombre de la Receta',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              TextField(
+                controller: _controllerNombreReceta,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Ingrese el nombre de la Receta',
+                  ),
+              ),
+              Size
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/drawerMenu.dart
@@ -0,0 +1,140 @@
+// ignore_for_file: use_build_context_synchronously
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:proyecto_recetario/database/db.dart';
+import 'package:proyecto_recetario/models/datosEstructura.dart';
+
+class MenuUsuario extends StatefulWidget {
+  final int id;
+  const MenuUsuario({super.key, required this.id});
+
+  @override
+  _MenuUsuarioState createState() => _MenuUsuarioState();
+}
+
+class _MenuUsuarioState extends State<MenuUsuario> {
+  Usuario? auxUser;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _cargarDatos();
+    });
+  }
+
+  Future<void> _cargarDatos() async {
+    try {
+      Usuario? datos = await RecetaBasesDeDatos.readIdUser(widget.id);
+      setState(() {
+        auxUser = datos;
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Fallo la carga de Datos de Usuario $e'),
+        ),
+      );
+    }
+  }
+
+  CircleAvatar _convertirImagen(String imgString) {
+    try {
+      if (imgString.isNotEmpty) {
+        return CircleAvatar(
+          radius: 70,
+          backgroundImage: FileImage(File(imgString)),
+        );
+      } else {
+        return const CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://cdn.pixabay.com/photo/2021/11/24/05/19/user-6820232_960_720.png'),
+          radius: 70,
+        );
+      }
+    } catch (e) {
+      return CircleAvatar(
+        radius: 70,
+        backgroundImage: MemoryImage(base64Decode(imgString)),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (auxUser == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return Drawer(
+        backgroundColor: Colors.white,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Row(
+                    children: [
+                      _convertirImagen(auxUser!.fotoUsuario),
+                      const SizedBox(
+                        width: 14.0,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '${auxUser!.nombreUsuario} ${auxUser!.apellidoUsuario}',
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            auxUser!.correoUsuario,
+                            style: const TextStyle(
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('Cerrar sesión'),
+                  onTap: () {
+                    Navigator.of(context).pushReplacementNamed('iniarSesion');
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    /*Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(auxUser?.fotoUsuario ??
+                    'https://cdn.pixabay.com/photo/2021/11/24/05/19/user-6820232_960_720.png'),
+                radius: 70,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+      */
+  }
+}
